d4d1dd1455aae39a127c916f60dd5e49a1d5c06c
Obed Cheung <obedcheung@Obeds-MacBook-Air.local>
2017-11-19 19:25:50 +0800

    Initial Commit

---
+++ sound(e)scape/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  sound(e)scape
+//
+//  Created by Obed Cheung on 19/11/2017.
+//  Copyright © 2017 obedcheung. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
